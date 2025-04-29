c3324c27f717e0559e35d01f13480a80e4153f3a
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-04-29 16:32:01 +0530

    add authentication routes skeleton for login and logout

---
+++ src/routes/auth/auth.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+//Need to implement yet.
+// router.route("/login").post(login);
+// router.route("/logout").post(authenticateToken, logout);
+
+export default router;
