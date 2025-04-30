d9d0e072392a9a2b3d79828ddfb416f5bf292b49
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-04-30 14:34:20 +0530

    add validation

--- src/models/blog/blog.js
+++ src/models/blog/blog.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true, unique: true },
-    slug: { type: String, required: true, unique: true },
+    title: { type: String, required: true, unique: true, trim: true },
+    slug: { type: String, required: true, unique: true, trim: true },
     thumbImage: {
       public_id: { type: String, required: true },
       secure_url: { type: String, required: true },
     },
-    content: { type: String, required: true },
+    content: { type: String, required: true, trim: true },
     author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
