2a23de8281f8fb1d8039ad75cf85b1fcb51633bb
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-04-30 14:16:49 +0530

    remove unused code

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { login, logout, register } from "../../controllers/auth/auth.js";
+import { login, logout } from "../../controllers/auth/auth.js";
 import { authenticateToken } from "../../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
-router.route("/signup").post(register);
 router.route("/login").post(login);
 router.route("/logout").post(authenticateToken, logout);
 
