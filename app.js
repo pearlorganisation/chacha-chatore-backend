6e4f4e28e7c7025be68b27b8caa3eb9289298594
jaipearl27 <jaix2793@gmail.com>
2025-05-26 14:19:36 +0530

    app updates

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import { errorHandler } from "./src/utils/error/errorHandler.js";
 
-const app = express(); 
+const app = express();
 
 app.use(
   cors({
     origin:
       process.env.NODE_ENV === "development"
         ? [
-            "http://localhost:3000",
-            "http://localhost:3002",
-            "http://localhost:3001",
-            "http://localhost:5173",
-            "http://localhost:5174",
-            "https://bimalinstitute.com",
-          ]
+          "http://localhost:3000",
+          "http://localhost:3002",
+          "http://localhost:3001",
+          "http://localhost:5173",
+          "http://localhost:5174",
+          "https://bimalinstitute.com",
+          "https://www.bimalinstitute.com",
+        ]
         : [
-          "https://bimalinstitute.com"
-        ], 
+          "https://bimalinstitute.com",
+          "https://www.bimalinstitute.com",
+        ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Specify allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
     credentials: true,
