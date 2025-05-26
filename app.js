95365ccb53140972fc972f61c9f4ef60904f51f6
jaipearl27 <jaix2793@gmail.com>
2025-05-26 14:22:44 +0530

    app updates

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(
           "http://localhost:3002",
           "http://localhost:3001",
           "http://localhost:5173",
-          "http://localhost:5174",
+          "http://localhost:5174", 
           "https://bimalinstitute.com",
           "https://www.bimalinstitute.com", 
         ]
